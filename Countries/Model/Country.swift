7f7e8ce1d9c31e3510405b430cc3036d66644a51
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-09-18 21:01:09 +0300

    setup project

---
+++ Countries/Model/Country.swift
@@ -0,0 +1,8 @@
+//
+//  Country.swift
+//  Countries
+//
+//  Created by Goodwasp on 18.09.2023.
+//
+
+import Foundation
---
+++ Countries/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Countries
+//
+//  Created by Goodwasp on 18.09.2023.
+//
+
+import Foundation
